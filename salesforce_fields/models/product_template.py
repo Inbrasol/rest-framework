fc99a0df4335bb48732b538a8460196679625e7f
Lolo Bravo <lolo.bravo@inbrasol.pe>
2025-01-18 14:37:26 -0500

    Add fields SF

--- salesforce_fields/models/product_template.py
+++ salesforce_fields/models/product_template.py
@@ -4,3 +4,5 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     sf_id = fields.Char(string="Salesforce ID", index=True, unique=True)
+    price_book_entry_id = fields.Char(string="Price Book Entry ID", index=True, unique=True)
+    price_book_id = fields.Char(string="Price Book ID", index=True, unique=True)
