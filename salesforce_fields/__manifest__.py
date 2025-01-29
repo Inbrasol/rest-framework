19be22afdde9b4f4c521434a7361e60efc88550c
Lolo Bravo <lolo.bravo@inbrasol.pe>
2025-01-28 21:16:38 -0500

    Fix - V4

--- salesforce_fields/__manifest__.py
+++ salesforce_fields/__manifest__.py
@@ -16,7 +16,7 @@
         "views/res_company_views.xml",
         "views/res_user_views.xml",
         "views/res_currency_views.xml",
-        #"views/res_partner_views.xml",
+        "views/res_partner_views.xml",
         "views/sale_order_views.xml",
         "views/crm_lead_views.xml",
         "views/product_template_views.xml",
