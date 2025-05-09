dfac99d42ef3d9a7afa4d31aaa88a36ec44e9c31
Lolo Bravo <lolo.bravo@inbrasol.pe>
2025-05-09 11:49:15 -0500

    Add Parent  field- User

--- salesforce_fields/models/salesforce_user.py
+++ salesforce_fields/models/salesforce_user.py
@@ -6,7 +6,8 @@ class SalesforceUser(models.Model):
 
     name = fields.Char(string='Name', required=True)
     user_id = fields.Many2one('res.users', string='User', required=True)
+    approval_user_id = fields.Many2one('res.users', string='Approval User')
+    partner_id = fields.Many2one('res.partner', string='Partner',related='user_id.partner_id', store=True)
     sf_id = fields.Char(string='Salesforce ID', required=True)
     sf_username = fields.Char(string='Salesforce Username', required=True)
     active = fields.Boolean(string='Active', default=True)
-    
